19511ef949035e124072b6bb02cc69f6b0923e84
jose <josealvarado.valdes98@gmail.com>
2026-05-12 12:33:14 -0400

    feat: inicializar proyecto MS Lista de Espera

---
+++ src/main/java/cl/rednorte/ms_lista_espera/MsListaEsperaApplication.java
@@ -0,0 +1,13 @@
+package cl.rednorte.ms_lista_espera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsListaEsperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsListaEsperaApplication.class, args);
+	}
+
+}
