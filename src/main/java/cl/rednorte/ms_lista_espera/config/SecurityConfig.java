e2eb5bb8396fc148b7d799f028f8551361b51877
jose <josealvarado.valdes98@gmail.com>
2026-05-13 12:53:00 -0400

    Rama de pruebas

--- src/main/java/cl/rednorte/ms_lista_espera/config/SecurityConfig.java
+++ src/main/java/cl/rednorte/ms_lista_espera/config/SecurityConfig.java
@@ -11,22 +11,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/actuator/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwkSetUri(
-                    "${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}"
-                ))
-            );
+   @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().permitAll() // ← permite todo sin autenticación
+        );
 
-        return http.build();
-    }
+    return http.build();
+}
 }
